53fb5ab3ff97c785b221d2734a7d1908406022aa
Nduong <nguyenduong21072009@gmail.com>
2025-09-30 16:38:12 +0700

    Update autorep.js

--- modules/commands/autorep.js
+++ modules/commands/autorep.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "CThanh",
 	description: "tạo autorep cho 1 tin nhắn",
-	commandCategory: "Thành Viên",
+	commandCategory: "Admin",
 	usages: "[autorep] => [text need autorep]",
 	cooldowns: 5,
 	dependencies: {
